856b39c6b709a34fa82101e6821a77462bf81152
Chan Kai Sum <samxunited@gmail.com>
2024-04-30 15:27:23 +0800

    remov some ntoes

--- Capstone/TC001.py
+++ Capstone/TC001.py
@@ -14,19 +14,13 @@ class TestMaybankPortal:
         self.driver.quit()
 
     def test_start_button_functionality(self):
-        """
-        Test case to verify the functionality of the "Start" button on the Maybank Customer Online Portal welcome page.
-        """
-        # Preconditions: Assume the user has navigated to the welcome page and the "Start" button is visible.
+
         wait = WebDriverWait(self.driver, 5)
         start_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".sc-eqUAAy")))
         start_button.click()
         # Simulate clicking the "Start" button
         self.driver.find_element(By.CSS_SELECTOR, ".sc-eqUAAy").click()
         assert self.driver.current_url == "https://getq04.qbe.ee/maybank" 
-
-        # Postconditions: Confirm that the new page is loaded successfully.
-        # Any cleanup or logging can be done here.
 
 # Example usage
 if __name__ == "__main__":
