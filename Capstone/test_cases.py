ed8fe7d41cdb39e0c5b42305b5497d9fd9e692ee
Chan Kai Sum <samxunited@gmail.com>
2024-05-06 21:07:52 +0800

    branch butto created

--- Capstone/test_cases.py
+++ Capstone/test_cases.py
@@ -72,5 +72,112 @@ class TestStates(unittest.TestCase):
         self.driver.quit()
 
 
+class TestMaybankDemos1(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.states = {
+            "Johor": ".sc-fPXMVe:nth-child(4) > div",
+            "Kedah": ".sc-fPXMVe:nth-child(5) > div",
+            "Kelantan": ".sc-fPXMVe:nth-child(6) > div",
+            "Melaka": ".sc-fPXMVe:nth-child(7) > div",
+            "Negeri Sembilan": ".sc-fPXMVe:nth-child(8) > div",
+            "Pahang": ".sc-fPXMVe:nth-child(9) > div",
+            "Perak": ".sc-fPXMVe:nth-child(10) > div",
+            "Perlis": ".sc-fPXMVe:nth-child(11) > div",
+            "Kuala Lumpur": ".sc-fPXMVe:nth-child(12) > div",
+            "Terengganu": ".sc-fPXMVe:nth-child(13) > div",
+            "Selangor": ".sc-fPXMVe:nth-child(14) > div",
+            "Penang": ".sc-fPXMVe:nth-child(15) > div",
+            "Putrajaya": ".sc-fPXMVe:nth-child(16) > div"
+        }
+
+
+    def test_demo_buttons1(self):
+        for state, css_selector in self.states.items():
+            try:
+                self.driver.get('https://getq04.qbe.ee/maybank')
+                wait = WebDriverWait(self.driver, 5)  # Wait for up to 5 seconds
+                start_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".sc-eqUAAy")))
+                start_button.click()
+                # Add assertions here to verify the functionality of the Start button
+                # Find the state button and click it
+                state_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+                state_button.click()
+                time.sleep(1)
+
+
+                # Click on Demo Maybank button
+                demo_button = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, '.sc-fPXMVe:nth-child(4)'))
+                )
+                demo_button.click()
+                time.sleep(1)
+                # Add assertions here to verify that you have navigated correctly or any other functionality
+                expected_url = 'https://getq04.qbe.ee/maybank/b'
+                self.assertEqual(self.driver.current_url, expected_url)
+
+            except Exception as e:
+                print(f"Test failed for state: {state}")
+                print(f"Error: {str(e)}")
+                pass 
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class TestMaybankDemos2(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.states = {
+            "Johor": ".sc-fPXMVe:nth-child(4) > div",
+            "Kedah": ".sc-fPXMVe:nth-child(5) > div",
+            "Kelantan": ".sc-fPXMVe:nth-child(6) > div",
+            "Melaka": ".sc-fPXMVe:nth-child(7) > div",
+            "Negeri Sembilan": ".sc-fPXMVe:nth-child(8) > div",
+            "Pahang": ".sc-fPXMVe:nth-child(9) > div",
+            "Perak": ".sc-fPXMVe:nth-child(10) > div",
+            "Perlis": ".sc-fPXMVe:nth-child(11) > div",
+            "Kuala Lumpur": ".sc-fPXMVe:nth-child(12) > div",
+            "Terengganu": ".sc-fPXMVe:nth-child(13) > div",
+            "Selangor": ".sc-fPXMVe:nth-child(14) > div",
+            "Penang": ".sc-fPXMVe:nth-child(15) > div",
+            "Putrajaya": ".sc-fPXMVe:nth-child(16) > div"
+        }
+
+
+    def test_demo_buttons1(self):
+        for state, css_selector in self.states.items():
+            try:
+                self.driver.get('https://getq04.qbe.ee/maybank')
+                wait = WebDriverWait(self.driver, 5)  # Wait for up to 5 seconds
+                start_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".sc-eqUAAy")))
+                start_button.click()
+                # Add assertions here to verify the functionality of the Start button
+                # Find the state button and click it
+                state_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+                state_button.click()
+                time.sleep(1)
+
+
+                # Click on Demo Maybank button
+                demo_button = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, '.sc-fPXMVe:nth-child(5)'))
+                )
+                demo_button.click()
+                time.sleep(1)
+                # Add assertions here to verify that you have navigated correctly or any other functionality
+                expected_url = 'https://getq04.qbe.ee/maybank/b'
+                self.assertEqual(self.driver.current_url, expected_url)
+
+
+            except Exception as e:
+                print(f"Test failed for state: {state}")
+                print(f"Error: {str(e)}")
+                pass 
+
+    def tearDown(self):
+        self.driver.quit()
+
+
 if __name__ == "__main__":
     unittest.main()
